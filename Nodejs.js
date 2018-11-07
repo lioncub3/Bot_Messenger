a74f131a0d5c94751e7a2a93d11d3e8d38d33092
Leon <leonid2000.11.11@gmail.com>
2018-11-07 14:43:34 +0200

    Update Webhook bot

--- Nodejs.js
+++ Nodejs.js
@@ -6,35 +6,61 @@ let express = require('express'),
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
  
-app.listen(80, () => console.log('Example app listening on port 80!'));
+app.listen(8080, () => console.log('Example app listening on port 8080!'));
 
 
 app.get('/', (req, res) => res.send('Hello World!'));
 
+// Creates the endpoint for our webhook 
+app.post('/webhook', (req, res) => {  
+ 
+    let body = req.body;
+  
+    // Checks this is an event from a page subscription
+    if (body.object === 'page') {
+  
+      // Iterates over each entry - there may be multiple if batched
+      body.entry.forEach(function(entry) {
+  
+        // Gets the message. entry.messaging is an array, but 
+        // will only ever contain one message, so we get index 0
+        let webhook_event = entry.messaging[0];
+        console.log(webhook_event);
+      });
+  
+      // Returns a '200 OK' response to all requests
+      res.status(200).send('EVENT_RECEIVED');
+    } else {
+      // Returns a '404 Not Found' if event is not from a page subscription
+      res.sendStatus(404);
+    }
+  
+  });
 
+  // Adds support for GET requests to our webhook
 app.get('/webhook', (req, res) => {
- 
-    // Токен верификации. Он должен быть строкой, состоящей из случайных символов
-    let VERIFY_TOKEN = "SOMETHING_RANDOM";
- 
-    // Разбор параметров запроса
+
+    // Your verify token. Should be a random string.
+    let VERIFY_TOKEN = "61FNvYv8kYDulgh";
+      
+    // Parse the query params
     let mode = req.query['hub.mode'];
     let token = req.query['hub.verify_token'];
     let challenge = req.query['hub.challenge'];
- 
-    // Проверка, имеются ли в запросе mode и token 
+      
+    // Checks if a token and mode is in the query string of the request
     if (mode && token) {
- 
-        // Проверка правильности mode и token
-        if (mode === 'subscribe' && token === VERIFY_TOKEN) {
- 
-            // Отправка токена challenge из запроса
-            console.log('WEBHOOK_VERIFIED');
-            res.status(200).send(challenge);
- 
-        } else {
-            // Отправка ответа '403 Forbidden' если верифицировать токен не удалось
-            res.sendStatus(403);
-        }
+    
+      // Checks the mode and token sent is correct
+      if (mode === 'subscribe' && token === VERIFY_TOKEN) {
+        
+        // Responds with the challenge token from the request
+        console.log('WEBHOOK_VERIFIED');
+        res.status(200).send(challenge);
+      
+      } else {
+        // Responds with '403 Forbidden' if verify tokens do not match
+        res.sendStatus(403);      
+      }
     }
-});
+  });
